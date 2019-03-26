0837f52bde95765c427de2c7dde6332b04ad04cd
Piyush <piyushupadhyay052@gmail.com>
2019-03-26 14:28:41 +0530

    Initial commit

---
+++ app/src/main/java/com/piyush052/locationstrategies/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.piyush052.locationstrategies
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    @SuppressLint("MissingPermission")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Acquire a reference to the system Location Manager
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        // Define a listener that responds to location updates
+        val locationListener = object : LocationListener {
+
+            override fun onLocationChanged(location: Location) {
+                // Called when a new location is found by the network location provider.
+                makeUseOfNewLocation(location)
+            }
+
+            override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+                textView.text = "Location  -- ${provider}, ${status}"
+
+            }
+
+            override fun onProviderEnabled(provider: String) {
+                textView.text = "Location  -- ${provider}"
+            }
+
+            override fun onProviderDisabled(provider: String) {
+                textView.text = "Location  -- ${provider}"
+
+            }
+        }
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, locationListener)
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+        locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0f, locationListener)
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun makeUseOfNewLocation(location: Location) {
+        textView.text = "Location  -- ${location.latitude}, ${location.longitude}"
+    }
+}
