3504613bad583e6c78d7d8e28d91432c1c031b72
Piyush <piyushupadhyay052@gmail.com>
2019-03-31 21:31:55 +0530

    Runtime permission is done

--- app/src/main/java/com/piyush052/locationstrategies/MainActivity.kt
+++ app/src/main/java/com/piyush052/locationstrategies/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.piyush052.locationstrategies
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
+
 
 class MainActivity : AppCompatActivity() {
 
     private var context: Context? = null
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION =  101
+
 
     var locationManager: LocationManager? = null
-    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,10 +33,96 @@ class MainActivity : AppCompatActivity() {
 
         // Acquire a reference to the system Location Manager
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        locationManager!!.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, locationListener)
+
+        checkPermission()
+
 
 
     }
+
+    @SuppressLint("MissingPermission")
+    fun startListeningLocation (){
+        locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+    }
+
+
+    private fun showRationaleDialog() {
+        AlertDialog.Builder(this)
+            .setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+                )
+            })
+            .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
+                Toast.makeText(this@MainActivity, "Provide the permission ", Toast.LENGTH_SHORT).show()
+            })
+            .setCancelable(false)
+            .setMessage("App need Location permission ")
+            .show()
+    }
+
+
+    fun checkPermission (){
+        if (ContextCompat.checkSelfPermission(context as MainActivity, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(context as MainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context as MainActivity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                showRationaleDialog()
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(context as MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+
+
+        }else{
+            startListeningLocation()
+        }
+
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startListeningLocation()
+                   // startLocationUpdates()
+                } else {
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        )
+                    ) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Please provide the permission ",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        showRationaleDialog()
+                    }
+                }
+            }
+        }
+    }
+
+
 
     private val locationListener: LocationListener
         get() = object : LocationListener {
@@ -42,14 +137,12 @@ class MainActivity : AppCompatActivity() {
 
             }
 
-            @SuppressLint("MissingPermission")
             override fun onProviderEnabled(provider: String) {
                 textView.append("\nonProviderEnabled  -- ${provider}")
               //  locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
 
             }
 
-            @SuppressLint("MissingPermission")
             override fun onProviderDisabled(provider: String) {
                textView.append( "\nonProviderDisabled  -- ${provider}")
               //  locationManager!!.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, locationListener)
