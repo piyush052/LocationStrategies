83be0a96514dcc18b080d829cf4d36b3dbb6804b
Piyush <piyushupadhyay052@gmail.com>
2019-05-18 19:53:34 +0530

    Network service changed

--- app/src/main/java/com/piyush052/locationstrategies/network/NetworkService.kt
+++ app/src/main/java/com/piyush052/locationstrategies/network/NetworkService.kt
@@ -19,49 +19,56 @@ import java.util.concurrent.TimeUnit
 
 class NetworkService {
 
+    var api: ApiEndPoints? = null
+
     fun getInstance(): ApiEndPoints {
-        val client = OkHttpClient().newBuilder()
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
-            })
-            .addInterceptor(object : Interceptor {
-                override fun intercept(chain: Interceptor.Chain): Response {
-                    val request = chain.request()
-                    Log.e("Interceptor", chain.request().url().toString())
+        if (api == null) {
+            val client = OkHttpClient().newBuilder()
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .addInterceptor(object : Interceptor {
+                    override fun intercept(chain: Interceptor.Chain): Response {
+                        val request = chain.request()
+                        Log.e("Interceptor", chain.request().url().toString())
 
-                    val url = request.url().toString()
-                    val newUrl = url.replace("/?","?")
-                    Log.e("New URL ", newUrl)
+                        val url = request.url().toString()
+                        val newUrl = url.replace("/?", "?")
+                        Log.e("New URL ", newUrl)
 
-                    val newRequest = request.newBuilder()
-                        .url(newUrl)
-                        .build()
-                    Log.e("Interceptor", newRequest.url().toString())
-                    return chain.proceed(newRequest)
-                }
+                        val newRequest = request.newBuilder()
+                            .url(newUrl)
+                            .build()
+                        Log.e("Interceptor", newRequest.url().toString())
+                        return chain.proceed(newRequest)
+                    }
 
-            })
-            .connectTimeout(4, TimeUnit.MINUTES)
-            .build()
+                })
+                .connectTimeout(4, TimeUnit.MINUTES)
+                .build()
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://api.traxsmart.in:5055")
-            .client(client)
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://api.traxsmart.in:5055")
+                .client(client)
 
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build()
-        return retrofit.create(ApiEndPoints::class.java)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+            return retrofit.create(ApiEndPoints::class.java)
+        } else {
+            return api as ApiEndPoints
+        }
     }
 
-     fun callLoginApi(request: com.piyush052.locationstrategies.network.Request<String>, networkResponse: NetworkResponse<String>){
+    fun callLoginApi(
+        request: com.piyush052.locationstrategies.network.Request<String>,
+        networkResponse: NetworkResponse<String>
+    ) {
         getInstance().sendDataToServer("https://j1rd28gar0.execute-api.ap-southeast-1.amazonaws.com/dev/organizations/5cd14207ff66d228b60586b4")
             .subscribeOn(Schedulers.newThread())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(){
-                request.response= Gson().toJson(it);
+            .subscribe() {
+                request.response = Gson().toJson(it);
                 networkResponse.onNetworkResponse(request)
-        Log.e("API res ", Gson().toJson(it))
-        }
+                Log.e("API res ", Gson().toJson(it))
+            }
     }
 }
